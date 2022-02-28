a11ba5b5956e16b10c4cd698cc120ed104b14e2d
Dinesh Cherukuri <93107793+dineshcherukuri815@users.noreply.github.com>
2022-02-28 06:57:30 +0000

    p1

--- p1original.c
+++ p1original.c
@@ -1,2 +1,29 @@
-
+#include<stdio.h>
+void input(int *num1, int *den1, int *num2, int *den2)
+{
+  printf("Enter the numerator of first num: ");
+  scanf("%d",num1);
+  printf("Enter the numerator of first num: ");
+  scanf("%d",den2);
+  printf("Enter the numerator of second num: ");
+  scanf("%d",num2);
+  printf("Enter the numerator of second num: ");
+  scanf("%d",den1);
+}
+void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
+{
+  *den3=den1*den2;
+  *num3=((*den3/den1)*num1+(*den3/den2)*num2);
+}
+void output(int num1, int den1, int num2, int den2, int num3, int den3)
+{
+  printf("sum of %d/%d and %d/%d is is%d/%d",num1,den1,num2,den2,num3,den3);
+}
+int main()
+{
+  int num1,den1,num2,den2,num3,den3;
+  input(&num1,&den1,&num2,&den2);
+  add(num1,den1,num2,den2,&num3,&den3);
+  output(num1,den1,num2,den2,num3,den3);
+  return 0;
 }
---
+++ p3original.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int input_n_and_r(int *n, int *r)
+{
+  printf("enter no of objects: ");
+  scanf("%d",n);
+  printf("enter the number of selected objects")
+}
